600900ca69f4bc00640446283db264c564263a9a
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-09 15:33:55 -0400

    :tada: Course done!!!

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,6 +16,54 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Pettagram - Tu app social de mascotas",
+      short_name: "Pettagram",
+      description:
+        "Con Petgram puedes encontrar y subir fotos de animales domésticos.",
+      orientation: "portrait",
+      display: "standalone",
+      start_url: "/",
+      scope: "/",
+      background_color: "#ffffff",
+      theme_color: "#2196f3",
+      crossorigin: "use-credentials", //can be null, use-credentials or anonymous
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+        },
+        {
+          src: path.resolve("src/assets/icon.png"),
+          size: "1024x1024", // you can also use the specifications pattern
+        },
+        {
+          src: path.resolve("src/assets/icon.png"),
+          size: "1024x1024",
+          purpose: "maskable",
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp("http://localhost:8080"),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
